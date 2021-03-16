41ed70ab038aa9e029163e8377e9a3af7f97d951
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-03-16 16:46:14 +0300

    created get,post,put

---
+++ Spring boot User demo/Spring-boot-User-demo/src/main/java/com/cobanogluhasan/springboot/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.cobanogluhasan.springboot.controller;
+
+import com.cobanogluhasan.springboot.exception.ResourceNotFoundException;
+import com.cobanogluhasan.springboot.repository.UserRepository;
+import model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping( "/api/v1/")
+public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //get all users
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+    //get user by id
+    @GetMapping("/users/{userId}")
+    public User getUserById(@PathVariable Long userId) {
+        return this.userRepository.findById(userId).orElseThrow(()->
+                new ResourceNotFoundException("User not found with id: " + userId));
+    }
+
+    //create user
+    @PostMapping("users")
+    public User createUser(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+
+    //update user
+    @PutMapping("/users/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody User user) {
+        User existingUser = this.userRepository.findById(userId).orElseThrow(()->
+                new ResourceNotFoundException("User not found with id: " + userId));
+
+        existingUser.setEmail(user.getEmail());
+        existingUser.setAddress(user.getAddress());
+        existingUser.setGsm(user.getGsm());
+        existingUser.setTckn(user.getTckn());
+        existingUser.setPassword(user.getPassword());
+
+        return this.userRepository.save(existingUser);
+    }
+
+    //delete user by id
+
+}
