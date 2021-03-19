e51e6a7b43c03784b7db89fe1f1412d287fc25f5
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-03-19 22:55:35 +0300

    fixing bugs

--- src/main/java/com/cobanogluhasan/springboot/SpringBootUserDemoApplication.java
+++ src/main/java/com/cobanogluhasan/springboot/SpringBootUserDemoApplication.java
@@ -2,9 +2,10 @@ package com.cobanogluhasan.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootUserDemoApplication {
+public class SpringBootUserDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootUserDemoApplication.class, args);
