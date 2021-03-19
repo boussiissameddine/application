dc662089413cdd25cdcd18ff05e5624c194079ff
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-03-19 20:49:38 +0300

    configure url

--- src/main/java/com/cobanogluhasan/springboot/controller/UserController.java
+++ src/main/java/com/cobanogluhasan/springboot/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping( "/api/v1/")
+@RequestMapping( "/")
 public class UserController  {
     private UserRepository userRepository;
 
