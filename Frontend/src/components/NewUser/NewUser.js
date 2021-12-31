fd55b1708526f8f39a57e67d9b63dea8e3e8678f
boussiissameddine <61164066+boussiissameddine@users.noreply.github.com>
2021-12-31 22:59:01 +0100

    TEST
    UPDATE

--- Frontend/src/components/NewUser/NewUser.js
+++ Frontend/src/components/NewUser/NewUser.js
@@ -41,27 +41,27 @@ class NewUser extends Component {
             <>
         <div className="NewUser">
         <h5>{this.props.text}</h5>
-        <form className="Form" onSubmit={this.handleSubmit} action="">
+        <form className="Form" onSubmit={this.handleSubmit} action=""><br>
             <label>ID-NUM
                 <input type="number" 
                 name="tcId" 
                 pattern="[0-9]{11}" 
                 required placeholder="NUM" 
                 value={this.state.tcId} //He
-                onChange={(event) => this.setState({tcId: event.target.value})}/></label><br></br>
+                onChange={(event) => this.setState({tcId: event.target.value})}/></label></br><br>
             <label>Email
                 <input type="email"
                  name="email" required 
                  placeholder="email" 
                  value={this.state.email}
-                 onChange={(event) => this.setState({email: event.target.value})}/></label><br></br>
+                 onChange={(event) => this.setState({email: event.target.value})}/></label></br><br>
             <label>Telephone
                 <input type="tel" 
                 name="phone" required 
                 placeholder="NUM" 
                 pattern="[0-9]{10}"
                 value={this.state.phone}
-                onChange={(event) => this.setState({phone: event.target.value})}/></label><br></br>
+                onChange={(event) => this.setState({phone: event.target.value})}/></label></br><br>
             <label>mot de passe
                 <input type="password" 
                 name="passwordHashed" 
@@ -70,12 +70,12 @@ class NewUser extends Component {
                 onChange={(event) => this.setState({password: event.target.value})}
                 placeholder="create password" 
                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-                title="Must contain one number and one uppercase and lowercase letter, and at least 8characters"/></label><br></br>
+                title="Must contain one number and one uppercase and lowercase letter, and at least 8characters"/></label></br><br>
             <label>Adresse
                 <input type="text" 
                 name="address" required
                 value={this.state.address}
-                onChange={(event) => this.setState({address: event.target.value})}/></label><br></br>
+                onChange={(event) => this.setState({address: event.target.value})}/></label></br>
             <input className="SaveButton" type="submit" value={this.props.buttonText} onClick={this.addNewUserHandler} />
         </form>
     </div>
